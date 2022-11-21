80d4e0b90d0d937c27a7aa681cdaa75504aabe2a
Daniel Murillo <sittingbout@gmail.com>
2022-11-21 16:18:58 +0100

    hotfix

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -12,16 +12,16 @@ export const Header = () => {
                     <NavLink to="/"><img className='h-14 mx-5 rounded-full' src='./images/logo3.png' alt='' /></NavLink>
                 </div>
                 <nav className='font-semibold font-oswald'>
-                    <NavLink to="Inicio" className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-secondary uppercase'>Inicio</NavLink>
-                    <NavLink to='Catalogo' className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-secondary uppercase'>Catálogo</NavLink>
-                    <NavLink to='Blog' className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-secondary uppercase'>Blog</NavLink>
-                    <NavLink to='About' className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-secondary uppercase'>Sobre Nosotros</NavLink>
-                    <NavLink to='Contact' className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-secondary uppercase'>Contacto</NavLink>
+                    <NavLink to="Inicio" className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-red-400 uppercase'>Inicio</NavLink>
+                    <NavLink to='Catalogo' className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-red-400 uppercase'>Catálogo</NavLink>
+                    <NavLink to='Blog' className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-red-400 uppercase'>Blog</NavLink>
+                    <NavLink to='About' className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-red-400 uppercase'>Sobre Nosotros</NavLink>
+                    <NavLink to='Contact' className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-red-400 uppercase'>Contacto</NavLink>
                 </nav>
                 <div className='flex items-center mx-4'>
                     <SearchIcon className='mr-5 text-slate-100 ' />
                     <Badge badgeContent="0" color="primary" >
-                        <NavLink href='/'><ShoppingCartIcon className=' text-secondary m-2 rounded-full p-1' fontSize='large' /></NavLink>
+                        <NavLink href='/'><ShoppingCartIcon className=' text-red-400 m-2 rounded-full p-1' fontSize='large' /></NavLink>
                     </Badge>
                 </div>
             </div>
