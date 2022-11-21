3604e855767ea5273235b61005ef1baea9fbda50
Daniel Murillo <sittingbout@gmail.com>
2022-11-21 18:00:22 +0100

    more visual improvements

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { MyRoutes } from './components/router/MyRoutes';
 
 function App() {
   return (
-    <div className="h-screen w-screen ">
+    <div>
 
       <MyRoutes />
 
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SearchIcon from '@mui/icons-material/Search';
-import { Badge } from '@mui/material';
+
 
 export const Header = () => {
     return (
         <div className='bg-slate-800'>
             <div className='flex justify-between border-b-2 p-4 items-center'>
                 <div className='hidden sm:block'>
-                    <NavLink to="/"><img className='h-14 mx-5 rounded-full' src='./images/logo3.png' alt='' /></NavLink>
+                    <NavLink to="/"><img className='h-16 mx-5 rounded-full' src='./images/logo3.png' alt='' /></NavLink>
                 </div>
                 <nav className='font-semibold font-oswald'>
                     <NavLink to="Inicio" className='mr-2 md:mr-8 xl:mr-10 text-sm text-white md:text-lg xl:text-xl hover:text-red-400 uppercase'>Inicio</NavLink>
@@ -20,9 +20,7 @@ export const Header = () => {
                 </nav>
                 <div className='flex items-center mx-4'>
                     <SearchIcon className='mr-5 text-slate-100 ' />
-                    <Badge badgeContent="0" color="primary" >
-                        <NavLink href='/'><ShoppingCartIcon className=' text-red-400 m-2 rounded-full p-1' fontSize='large' /></NavLink>
-                    </Badge>
+                    <NavLink href='/'><ShoppingCartIcon className=' text-red-400 m-2 rounded-full p-1' fontSize='large' /></NavLink>
                 </div>
             </div>
         </div>
